e0d00a890c52e0343cc1d4d634e0817d7fc9a071
albelix <albelix@yandex.ru>
2018-12-01 08:50:55 +0300

    Add files via upload

--- auc/tests.py
+++ auc/tests.py
@@ -7,5 +7,18 @@ from .models import Constants
 class PlayerBot(Bot):
 
     def play_round(self):
-        yield (pages.MyPage)
+        yield (pages.Role)
+        yield (pages.Valuation)
+        if "player" == "Seller":
+            yield (pages.Subrole, {"subrole": "auctioneer"})
+#            yield (pages.Bids_Sellers, {'sellerbid': c(4.2)})
+#            if "subrole" == "auctioneer":
+#                yield (pages.No_Bids_Sellers)
+            assert self.group.bs_auc_ind == 0
+        elif "player" == "Buyer":
+            yield (pages.Subrole, {"subrole": "participant"})
+#            if "subrole" == "participant":
+            yield (pages.Bids_Buyers, {'buyerbid': c(4.2)})
+#                yield (pages.No_Bids_Buyers)
         yield (pages.Results)
+
